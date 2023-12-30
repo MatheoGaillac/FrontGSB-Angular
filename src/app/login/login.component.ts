04db240800c9108f4812869f15083d38210d65d4
MatheoGaillac <glc.matheo@gmail.com>
2023-12-30 09:53:40 +0100

    Fix LoginForm : Empty input when submit + error CSS

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,20 +13,23 @@ import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angula
 })
 
 export class LoginComponent {
-  user_id: FormControl = new FormControl('');
-  password: FormControl = new FormControl('');
+  user_id: FormControl = new FormControl('', Validators.required);
+  password: FormControl = new FormControl('', Validators.required);
+  formSubmitted: boolean = false;
 
   constructor(private loginService: GsbLoginService) {}
 
   onSubmit() {
+    this.formSubmitted = true;
+
     if (this.user_id.invalid || this.password.invalid) {
-      // Le formulaire n'est pas valide, n'envoie pas les informations
       return;
     }
-  
+
     this.loginService.serviceEnvoieLogin(
       this.user_id.value,
       this.password.value
     );
   }
 }
+
