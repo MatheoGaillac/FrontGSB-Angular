e83676f7132f59c7348f30ef0364584202e23e91
MatheoGaillac <glc.matheo@gmail.com>
2023-12-30 11:28:26 +0100

    Feat : Toggle visibility password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,17 +2,13 @@ import { MenuComponent } from '../menu/menu.component';
 import { GsbLoginService } from '../service/gsb-login.service';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MenuComponent],
+  imports: [CommonModule, ReactiveFormsModule, MenuComponent, MatIconModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
@@ -20,6 +16,7 @@ export class LoginComponent {
   user_id: FormControl = new FormControl('', Validators.required);
   password: FormControl = new FormControl('', Validators.required);
   formSubmitted: boolean = false;
+  showPassword: boolean = false;
 
   constructor(private loginService: GsbLoginService) {}
 
@@ -37,5 +34,9 @@ export class LoginComponent {
 
   getErrorMessage() {
     return this.loginService.getErrorStatus();
+  }
+
+  toggleVisibility(){
+    this.showPassword = !this.showPassword;
   }
 }
