454b87f0501df8f288efa70d06a581b8beaf996c
Matheo <matheo.gaillac@lmdsio.fr>
2024-04-05 11:47:07 +0200

    Feat : Add Service and Model for Praticien

---
+++ src/app/metier/praticien.ts
@@ -0,0 +1,28 @@
+export class Praticien {
+
+  public id_praticien: number = 0;
+  public id_type_praticien: number = 0;
+  public nom_praticien: string = "";
+  public prenom_praticien: string = "";
+  public adresse_praticien: string = "";
+  public cp_praticien: number = 0;
+  public ville_praticien: string = "";
+  public coef_notoriete: number = 0;
+
+  constructor(json?: any) {
+    if (json) {
+      this.id_praticien = json.id_praticien;
+      this.id_type_praticien = json.id_type_praticien;
+      this.nom_praticien = json.nom_praticien;
+      this.prenom_praticien = json.prenom_praticien;
+      this.adresse_praticien = json.adresse_praticien;
+      this.cp_praticien = json.cp_praticien;
+      this.ville_praticien = json.ville_praticien;
+      this.coef_notoriete = json.coef_notoriete;
+    }
+  }
+
+  public getPraticien(): string {
+    return '${this.id_praticien}';
+  }
+}
---
+++ src/app/service/praticien.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {Praticien} from "../metier/praticien";
+@Injectable({
+  providedIn: 'root'
+})
+export class PraticienService {
+
+  constructor() { }
+}
