62269ab34a0ccf0a0015a3000571006d91001390
MatheoGaillac <glc.matheo@gmail.com>
2024-04-23 11:04:23 +0200

    Toggle visibility of invitation

--- src/app/metier/inviter.ts
+++ src/app/metier/inviter.ts
@@ -1,20 +1,26 @@
 export class Inviter {
-
 	public id_activite_compl: number = 0;
 	public id_praticien: number = 0;
 	public specialiste: string = "";
+	public date_activite: string = "";
+	public lieu_activite: string = "";
+	public theme_activite: string = "";
+	public motif_activite: string = "";
   
 	constructor(json?: any) {
 	  if (json) {
 		this.id_activite_compl = json.id_activite_compl;
 		this.id_praticien = json.id_praticien;
 		this.specialiste = json.specialiste;
-
+		this.date_activite = json.date_activite;
+		this.lieu_activite = json.lieu_activite;
+		this.theme_activite = json.theme_activite;
+		this.motif_activite = json.motif_activite;
 	  }
 	}
   
 	public getInvitation(): string {
-        return `${this.id_activite_compl}, ${this.id_praticien}`;
+	  return `${this.id_activite_compl}, ${this.id_praticien}`;
 	}
   }
   
--- src/app/praticiens/affiche-praticien/affiche-praticien.component.ts
+++ src/app/praticiens/affiche-praticien/affiche-praticien.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../menu/menu.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PraticienService } from '../../service/praticien.service';
 import { Praticien } from '../../metier/praticien';
+import { InviterService } from '../../service/inviter.service';
+import { Inviter } from '../../metier/inviter';
 
 @Component({
   selector: 'app-affiche-praticien',
@@ -16,8 +19,9 @@ import { Praticien } from '../../metier/praticien';
 export class AffichePraticienComponent {
   public id_praticien: number = 0;
   public praticien: Praticien | null = null;
+  public inviter: Inviter | null = null;
 
-  constructor(private praticien_api: PraticienService, private route: ActivatedRoute) {}
+  constructor(private praticien_api: PraticienService, private inviter_api: InviterService, private route: ActivatedRoute) {}
 
   ngOnInit(): void{
     this.id_praticien = parseInt(this.route.snapshot.paramMap.get('id_praticien')!);
@@ -27,5 +31,10 @@ export class AffichePraticienComponent {
       },
       error => console.log('Erreur appel API')
     );
+    this.inviter_api.listInvitationPraticien(this.id_praticien)
+  }
+
+  getListInvitationPraticien() {
+    return this.inviter_api.appels_termines;
   }
 }
