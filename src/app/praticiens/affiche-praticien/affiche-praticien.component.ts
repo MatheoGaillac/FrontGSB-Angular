a5fc465efbdb1d9d9f96e56455bb19e06c8338dd
MatheoGaillac <glc.matheo@gmail.com>
2024-04-24 14:23:07 +0200

    Feat: U can now delete invitation

--- src/app/praticiens/affiche-praticien/affiche-praticien.component.ts
+++ src/app/praticiens/affiche-praticien/affiche-praticien.component.ts
@@ -20,17 +20,24 @@ export class AffichePraticienComponent {
   public praticien: Praticien | null = null;
   public inviter: Inviter | null = null;
 
-  constructor(private praticien_api: PraticienService, private inviter_api: InviterService, private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private praticien_api: PraticienService,
+    private inviter_api: InviterService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void{
-    this.id_praticien = parseInt(this.route.snapshot.paramMap.get('id_praticien')!);
+  ngOnInit(): void {
+    this.id_praticien = parseInt(
+      this.route.snapshot.paramMap.get('id_praticien')!
+    );
     this.praticien_api.getUnPraticien(this.id_praticien).subscribe(
       (data: Praticien) => {
         this.praticien = data;
       },
-      error => console.log('Erreur appel API')
+      (error) => console.log('Erreur appel API')
     );
-    this.inviter_api.listInvitationPraticien(this.id_praticien)
+    this.inviter_api.listInvitationPraticien(this.id_praticien);
   }
 
   getListInvitationPraticien() {
@@ -38,6 +45,35 @@ export class AffichePraticienComponent {
   }
 
   modifierInvitation(id_praticien: number, id_activite_compl: number) {
-    this.router.navigate(['/praticiens/modifier', id_praticien, id_activite_compl]);
+    this.router.navigate([
+      '/praticiens/modifier',
+      id_praticien,
+      id_activite_compl,
+    ]);
+  }
+
+  confirmDelete(idActiviteCompl: number, idPraticien: number): void {
+    const isConfirmed = confirm(
+      'Êtes-vous sûr de vouloir supprimer cette invitation ?'
+    );
+
+    if (isConfirmed) {
+      this.inviter_api.deleteInvitation(idActiviteCompl, idPraticien).subscribe(
+        () => {
+          console.log('Invitation supprimée avec succès !');
+          this.reloadInvitations();
+        },
+        (error) => {
+          console.error(
+            "Erreur lors de la suppression de l'invitation :",
+            error
+          );
+        }
+      );
+    }
+  }
+
+  reloadInvitations(): void {
+    this.inviter_api.listInvitationPraticien(this.id_praticien);
   }
 }
--- src/app/service/inviter.service.ts
+++ src/app/service/inviter.service.ts
@@ -9,11 +9,11 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class InviterService {
-  private inviter: Inviter = new Inviter;
+  private inviter: Inviter = new Inviter();
   private _reponses = new BehaviorSubject<Inviter[]>([]);
   readonly appels_termines = this._reponses.asObservable();
   public listInvitation: Inviter[] = [];
-  public dataStore: { inviter: Inviter[] } = {inviter: []};
+  public dataStore: { inviter: Inviter[] } = { inviter: [] };
 
   constructor(
     private http: HttpClient,
@@ -21,14 +21,14 @@ export class InviterService {
     private router: Router
   ) {}
 
-  
   listInvitationPraticien(id_praticien: number) {
     const headers = new HttpHeaders({
       Authorization: 'Bearer ' + this.gsb_api.recupererBearer(),
     });
     return this.http
       .get<Inviter[]>(
-        'http://127.0.0.1:8000/api/praticien/getInvitationPraticien/' + id_praticien
+        'http://127.0.0.1:8000/api/praticien/getInvitationPraticien/' +
+          id_praticien
       )
       .subscribe(
         (data) => {
@@ -41,67 +41,100 @@ export class InviterService {
 
   chargeInvitation(id_praticien: number, id_activite_compl: number) {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.gsb_api.recupererBearer()
+      Authorization: 'Bearer ' + this.gsb_api.recupererBearer(),
     });
-    return this.http.get<Inviter>('http://127.0.0.1:8000/api/praticien/getUneInvitation/' +
-    id_praticien + '/' + id_activite_compl, { headers: headers });
+    return this.http.get<Inviter>(
+      'http://127.0.0.1:8000/api/praticien/getUneInvitation/' +
+        id_praticien +
+        '/' +
+        id_activite_compl,
+      { headers: headers }
+    );
   }
 
-  createInvitation(id_activite_compl: number, id_praticien: number, specialiste: string) {
+  createInvitation(
+    id_activite_compl: number,
+    id_praticien: number,
+    specialiste: string
+  ) {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.gsb_api.recupererBearer()
+      Authorization: 'Bearer ' + this.gsb_api.recupererBearer(),
     });
 
     const requestObject = {
-      "id_activite_compl": id_activite_compl,
-      "id_praticien": id_praticien,
-      "specialiste": specialiste
+      id_activite_compl: id_activite_compl,
+      id_praticien: id_praticien,
+      specialiste: specialiste,
     };
 
-    this.http.post<Inviter>(`http://127.0.0.1:8000/api/praticien/addInvitation`
-      , requestObject, {headers: headers})
+    this.http
+      .post<Inviter>(
+        `http://127.0.0.1:8000/api/praticien/addInvitation`,
+        requestObject,
+        { headers: headers }
+      )
       .subscribe(
-        data => {
+        (data) => {
           this.inviter = new Inviter(data);
           this.dataStore.inviter.push(this.inviter);
           this._reponses.next(this.dataStore.inviter);
-          this.router.navigate(['praticiens/liste/'+ id_praticien]);
-          console.log("Appel réussi");
+          this.router.navigate(['praticiens/liste/' + id_praticien]);
+          console.log('Appel réussi');
         },
-        error => {
+        (error) => {
           if (error.status === 500) {
             console.log("L'invitation existe déjà !");
           } else {
-            console.log("Erreur Appel API", error);
+            console.log('Erreur Appel API', error);
           }
         }
       );
   }
 
-  updateInviter(id_activite_compl: number, id_praticien: number, old_id_activite_compl: number) {
+  updateInviter(
+    id_activite_compl: number,
+    id_praticien: number,
+    old_id_activite_compl: number
+  ) {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + this.gsb_api.recupererBearer()
+      Authorization: 'Bearer ' + this.gsb_api.recupererBearer(),
     });
 
     const requestObject = {
-      "id_activite_compl": id_activite_compl,
-      "id_praticien": id_praticien,
-      "old_id_activite_compl": old_id_activite_compl,
+      id_activite_compl: id_activite_compl,
+      id_praticien: id_praticien,
+      old_id_activite_compl: old_id_activite_compl,
     };
 
-    this.http.post<Inviter>(`http://127.0.0.1:8000/api/praticien/updateInvitation/`
-      , requestObject, {headers: headers})
+    this.http
+      .post<Inviter>(
+        `http://127.0.0.1:8000/api/praticien/updateInvitation/`,
+        requestObject,
+        { headers: headers }
+      )
       .subscribe(
-        data => {
+        (data) => {
           this.inviter = new Inviter(data);
           this.dataStore.inviter.push(this.inviter);
           this._reponses.next(this.dataStore.inviter);
-          this.router.navigate(['praticiens/liste/'+ id_praticien]);
-          console.log("Appel réussi");
+          this.router.navigate(['praticiens/liste/' + id_praticien]);
+          console.log('Appel réussi');
         },
-        error => {
-          console.log("Erreur Appel API", error);
+        (error) => {
+          console.log('Erreur Appel API', error);
         }
       );
+  }
+
+  deleteInvitation(idActiviteCompl: number, idPraticien: number) {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.gsb_api.recupererBearer(),
+    });
+
+    return this.http.post<any>(
+      `http://127.0.0.1:8000/api/praticien/deleteInvitation`,
+      { id_activite_compl: idActiviteCompl, id_praticien: idPraticien },
+      { headers: headers }
+    );
   }
 }
