bf1211476b048fae98e6eb4c78c8bacd2c56df9b
MatheoGaillac <glc.matheo@gmail.com>
2024-04-24 10:43:41 +0200

    Added model for a table

---
+++ src/app/metier/activite-compl.ts
@@ -0,0 +1,21 @@
+export class ActiviteCompl {
+  public id_activite_compl: number = 0;
+  public date_activite: string = '';
+  public lieu_activite: string = '';
+  public theme_activite: string = '';
+  public motif_activite: string = '';
+
+  constructor(json?: any) {
+    if (json) {
+      this.id_activite_compl = json.id_activite_compl;
+      this.date_activite = json.date_activite;
+      this.lieu_activite = json.lieu_activite;
+      this.theme_activite = json.theme_activite;
+      this.motif_activite = json.motif_activite;
+    }
+  }
+
+  public getActiviteCompl(): string {
+	return '${this.id_activite_compl}';
+  }
+}
