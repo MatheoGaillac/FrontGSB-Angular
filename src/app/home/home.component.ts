99e1c558753d9fcd5441540639cb560ce4b8a6ba
Matheo <matheo.gaillac@lmdsio.fr>
2024-05-02 17:02:48 +0200

    Feat : Upgrade the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
+import {GsbLoginService} from "../service/gsb-login.service";
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private gsbLoginService: GsbLoginService, private renderer: Renderer2) {
+  }
 
+  isLoggedIn(): boolean {
+    return this.gsbLoginService.getIsLogin();
+  }
+
+  isAdmin(): boolean {
+    return this.gsbLoginService.visiteurType() === 'A';
+  }
 }
